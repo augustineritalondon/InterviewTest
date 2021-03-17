08acfc2a7c76e3559470f7ecd12df721c411d2bf
augustineritalondon <augustineritalondon@gmail.com>
2021-03-17 09:01:59 +0000

    project layout with pagination limiting the number of users per page

--- main.js
+++ main.js
@@ -1,31 +1,35 @@
 new Vue({
-    el: "#app",
-    data: {
-      fullName: "",
-      email: "",
-      location: "",
-      phone: "",
-      image: ""
+  el: "#app",
+  data: {
+    users: [],
+    page: [],
+    perPage: 4,
+    currentPage: 0,
+    maxPages: 0
+  },
+  methods: {
+    fetchData: function () {
+      fetch('https://randomuser.me/api/?results=20')
+        .then((response) => response.json())
+        .then((data) => {
+          this.users = data.results; // all user data
+          this.page = this.users.slice(this.currentPage, this.perPage) // page user data
+          this.maxPages = Math.ceil(data.results.length / 4); // maximum pages
+        });
     },
-    methods: {
-      fetchData: function () {
-        fetch('https://randomuser.me/api/?results=1000')
-          .then((response) => response.json())
-          .then((data) => {
-            let info = data.results[0];
-            this.fullName = `${(info.name.first)} ${(
-              info.name.last
-            )}`;
-            this.email = info.email;
-            this.location = `${(info.location.street.number)} ${(info.location.street.name)} ${(
-              info.location.city)} ${(info.location.state)}`;
-            this.phone = info.phone;
-            this.image = info.picture.medium;
-            console.log(info);
-          });
-      }
+    next: function () {
+      if ((this.currentPage + 1) >= this.maxPages) return; // to prevent forward if at the end of the array
+      this.currentPage = this.currentPage + 1; // counter
+      this.page = this.users.slice(this.currentPage * 4, (this.currentPage * 4) + this.perPage);
     },
-    created: function () {
-      this.fetchData();
-    } 
-  });
+
+    previous: function () {
+      if ((this.currentPage - 1) < 0) return; // to prevent back if already at the begining of the array
+      this.currentPage = this.currentPage - 1; // counter
+      this.page = this.users.slice(this.currentPage * 4, (this.currentPage * 4) + this.perPage);
+    }
+  },
+  created: function () {
+    this.fetchData();
+  }
+});
